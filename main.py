4eb110f483eb0776cf01455aa50aa27871d72043
fusoreur <fusoreur@gmail.com>
2021-07-09 18:07:22 +0200

    récupération de tous les liens wikipédia

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 home = 'https://fr.wikipedia.org'
-
-html_text = requests.get(home).text
-soup = BeautifulSoup(html_text, 'lxml')
-for a in soup.find_all('a', href=True):
-    if a['href'][0] == '/' and a['href'][1] == 'w':
-        print(home + a['href'])
+all_link = ['']
+for i in range(50):
+    html_text = requests.get(home + all_link[i]).text
+    soup = BeautifulSoup(html_text, "lxml")
+    for a in soup.find_all('a', href=True):
+        if a['href'][0] == '/' and a['href'][1] == 'w' and a['href'][2] == 'i':
+            find = False
+            for link in all_link:
+                if link == a['href']:
+                    find = True
+                    break
+            if not find:
+                all_link.append(a['href'])
+    print('[{}]'.format(i), len(all_link))
+print(len(all_link))
